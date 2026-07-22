ce514b1e99fef29857487c62c51ef08375152f06
Kenji Saito <ken-yo@mbr.nifty.com>
2026-07-23 00:17:50 +0900

    Bumps node modules

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack.js';
+import * as cdk from 'aws-cdk-lib';
 
 const app = new cdk.App();
 const name = 'astro-lambda-ssr-example';
